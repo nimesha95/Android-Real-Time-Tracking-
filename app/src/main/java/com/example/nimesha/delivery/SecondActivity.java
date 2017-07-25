bcba0e864651c615f795052f66a33c4ce1e36070
nimesha95 <nimesha95@live.com>
2017-07-25 22:42:08 +0530

    First push from AndroidStudio Git Sync

--- app/src/main/java/com/example/nimesha/delivery/SecondActivity.java
+++ app/src/main/java/com/example/nimesha/delivery/SecondActivity.java
@@ -2,12 +2,52 @@ package com.example.nimesha.delivery;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private static final String TAG = "Database1";
+
+    TextView text1;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("condition");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        text1 = (TextView) findViewById(R.id.textView1);
+
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                text1.setText(value);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 }
