4a04a1e2f75bf4e5760389140f614e9c91c3d95c
nimesha95 <nimesha95@live.com>
2017-10-17 15:25:31 +0530

    let go of the mapbox XD :(

--- app/src/main/java/com/example/nimesha/delivery/decision_point.java
+++ app/src/main/java/com/example/nimesha/delivery/decision_point.java
@@ -24,7 +24,7 @@ public class decision_point extends AppCompatActivity {
         CurJobBtn.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
-                        Intent intent = new Intent(decision_point.this, CurrentJobs.class);
+                        Intent intent = new Intent(decision_point.this, MapboxActivity.class);
                         startActivity(intent);
 
                     }
